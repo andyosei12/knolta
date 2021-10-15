b21ae4a01854df6f8c8d908b7e9ccb387d6d9593
Andy-Osei <andrew.owusu@novanest.net>
2021-10-15 14:29:22 +0000

    feat-routes definiition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+import Appointment from "./pages/Appointment";
+import Events from "./pages/Events";
+import Executives from "./pages/Executives";
+import Home from "./pages/Home";
+import Liturgy from "./pages/Liturgy";
 
 function App() {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/events">
+          <Events />
+        </Route>
+        <Route path="/appointment">
+          <Appointment />
+        </Route>
+        <Route path="/executives">
+          <Executives />
+        </Route>
+        <Route path="/liturgy">
+          <Liturgy />
+        </Route>
+      </Switch>
+    </MainLayout>
+  );
 }
 
 export default App;
---
+++ src/pages/Liturgy.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Liturgy = () => {
+  return <div>Liturgy page</div>;
+};
+
+export default Liturgy;
