e441642124b43360cc02465632466802750e139f
Andy-Osei <andrew.owusu@novanest.net>
2021-10-27 11:00:10 +0000

    feat: added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import React, { useState, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import MainLayout from "./layout/MainLayout";
-import EventForm from "./pages/EventForm";
-import Appointment from "./pages/Appointment";
-import Events from "./pages/Events";
-import Executives from "./pages/Executives";
+
 import Home from "./pages/Home";
-import Liturgy from "./pages/Liturgy";
-import EditEvent from "./pages/EditEvent";
 import DeleteModal from "./components/ui/DeleteModal";
-import Executiveform from "./pages/ExecutiveForm";
-import EditExecutive from "./pages/EditExecutive";
-import AppointmentForm from "./pages/AppointmentForm";
-import EditAppointment from "./pages/EditAppointment";
+import Loader from "./components/ui/Loader";
+
+const EventForm = React.lazy(() => import("./pages/EventForm"));
+const Events = React.lazy(() => import("./pages/Events"));
+const EditEvent = React.lazy(() => import("./pages/EditEvent"));
+
+const Appointment = React.lazy(() => import("./pages/Appointment"));
+const AppointmentForm = React.lazy(() => import("./pages/AppointmentForm"));
+const EditAppointment = React.lazy(() => import("./pages/EditAppointment"));
+
+const Executives = React.lazy(() => import("./pages/Executives"));
+const ExecutiveForm = React.lazy(() => import("./pages/ExecutiveForm"));
+const EditExecutive = React.lazy(() => import("./pages/EditExecutive"));
+
+const Liturgy = React.lazy(() => import("./pages/Liturgy"));
 
 function App() {
   const [eventId, setEventId] = useState("");
@@ -27,44 +33,52 @@ function App() {
   };
   return (
     <MainLayout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/events" exact>
-          <Events onConfirmDelete={confirmDeleteHandler} />
-        </Route>
-        <Route path="/events/create">
-          <EventForm />
-        </Route>
-        <Route path="/events/:eventId/edit">
-          <EditEvent />
-        </Route>
-        <Route path="/appointments" exact>
-          <Appointment onConfirmDelete={confirmAppointmentDeleteHandler} />
-        </Route>
-        <Route path="/appointments/create">
-          <AppointmentForm />
-        </Route>
-        <Route path="/appointments/:appointmentId/edit">
-          <EditAppointment />
-        </Route>
-        <Route path="/executives" exact>
-          <Executives />
-        </Route>
-        <Route path="/executives/create">
-          <Executiveform />
-        </Route>
-        <Route path="/executives/:executiveId/edit">
-          <EditExecutive />
-        </Route>
-        <Route path="/liturgy">
-          <Liturgy />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div>
+            <Loader />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/events" exact>
+            <Events onConfirmDelete={confirmDeleteHandler} />
+          </Route>
+          <Route path="/events/create">
+            <EventForm />
+          </Route>
+          <Route path="/events/:eventId/edit">
+            <EditEvent />
+          </Route>
+          <Route path="/appointments" exact>
+            <Appointment onConfirmDelete={confirmAppointmentDeleteHandler} />
+          </Route>
+          <Route path="/appointments/create">
+            <AppointmentForm />
+          </Route>
+          <Route path="/appointments/:appointmentId/edit">
+            <EditAppointment />
+          </Route>
+          <Route path="/executives" exact>
+            <Executives />
+          </Route>
+          <Route path="/executives/create">
+            <ExecutiveForm />
+          </Route>
+          <Route path="/executives/:executiveId/edit">
+            <EditExecutive />
+          </Route>
+          <Route path="/liturgy">
+            <Liturgy />
+          </Route>
+        </Switch>
+      </Suspense>
       {deleteModal && (
         <DeleteModal eventId={eventId} appointmentId={appointmentId} />
       )}
